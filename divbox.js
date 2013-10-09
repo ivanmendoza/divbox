5a8b8c3644c15ea18dd1ba08d2cc4eac564e19c8
imendoza <dev@ivanmendoza.net>
2013-10-09 00:59:09 -0600

    New parameters to support close-button (string, jquery selector) and callbacks (array of objects, each object with "trigger" and "callback" values). Callbacks not implemented yet. Styles-source added. SNBX added in order to support style-compilation. Files cleaned up

--- divbox.js
+++ divbox.js
@@ -1,6 +1,7 @@
 (function($){
-	$.fn.divBox = function(content){
+	$.fn.divBox = function(content, closeButton, callbacks){
 		var $this = this;
+		var closeButton = closeButton || '.close-box';
 		var $divboxTarget = $this;
 		if(content){
 			$content = $(content, $this);
@@ -20,6 +21,12 @@
 			var $divboxContainer = 	$("<div>").attr("class", "divboxContainer").css({'position':'absolute', 'display':'none', 'top':'0', 'bottom':'0', 'left':'0', 'right':'0', 'z-index':'1001'});
 			var $divbox = 			$("<div>").attr("class", "divbox").css({'position':'absolute', 'background':divboxColor, 'top':'0', 'bottom':'0', 'left':'0', 'right':'0', 'z-index':'1002'}).fadeTo(0, .9);
 				$divboxContent = 	$("<div>").attr("class", "divboxContent").css({'position':'absolute', 'top':'30px', 'bottom':'30px', 'left':'30px', 'right':'30px', 'z-index':'1003'});
+			
+			$(closeButton).each(function(){
+				$(this).on("click", function(){
+					deleteDivbox($divboxContainer.parent());
+				});
+			});
 			
 			if($content){
 				$content.show();
